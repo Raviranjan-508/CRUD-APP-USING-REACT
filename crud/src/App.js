7f2f452eeb68313af743187fb2f27e7cd3175557
Raviranjan <ravikrcse2018@gmail.com>
2022-11-29 20:46:05 +0530

    data sis showing

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,10 +1,72 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import { MDBTable, MDBTableHead, MDBTableBody, MDBRow, MDBCol, MDBContainer } from "mdb-react-ui-kit";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadUsersData();
+  }, []);
+
+
+  // Fetch data from db.json
+  const loadUsersData = async () => {
+    return await axios
+      .get('http://localhost:3000/users')
+      .then((res) => {
+        console.log(res.data)
+        setData(res.data)
+      })
+      .catch((error) => console.log(error))
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <MDBContainer>
+      <div style={{ marginTop: "100px" }} >
+        <h2>Search , Filter , Sort and Pagination using db.json</h2>
+        <MDBRow>
+          <MDBCol size="12">
+            <MDBTable>
+              <MDBTableHead dark>
+                <tr>
+                  <th scope='col'>No.</th>
+                  <th scope='col'>Name</th>
+                  <th scope='col'>Email</th>
+                  <th scope='col'>Phone</th>
+                  <th scope='col'>Address</th>
+                  <th scope='col'>Status</th>
+                </tr>
+              </MDBTableHead>
+              {
+                data.length === 0 ? (
+                  <MDBTableBody className="align-center mb-0">
+                    <tr>
+                      <td colSpan={8} className="text-center mb-0">No data found</td>
+                    </tr>
+                  </MDBTableBody>
+                ) : (
+                  data.map((item, index) => (
+                    <MDBTableBody key={index}>
+                      <tr>
+                        <th scope='row'>{index + 1}</th>
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                        <td>{item.address}</td>
+                        <td>{item.status}</td>
+                      </tr>
+                    </MDBTableBody>
+                  ))
+                )
+              }
+            </MDBTable>
+          </MDBCol>
+        </MDBRow>
+      </div>
+    </MDBContainer>
   );
 }
 
